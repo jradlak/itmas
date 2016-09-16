0b98b4f3cb1c61ca264bfc924844188292cbf693
Jakub Radlak <jradlak@gmail.com>
2016-09-16 19:36:50 +0200

    showing all types of tasks in user details form

--- itmas_server/src/main/resources/static/app/components/users/users-detail.controller.js
+++ itmas_server/src/main/resources/static/app/components/users/users-detail.controller.js
@@ -2,10 +2,10 @@
 
 angular.module('itmas_server')
     .controller('UsersDetailController', function($scope, $stateParams, User) {
-        $scope.user = {};
+        $scope.user = {};       
         $scope.load = function (login) {
             User.get({login: login}, function(result) {
-                $scope.user = result;
+                $scope.user = result;                
             });
         };
 
